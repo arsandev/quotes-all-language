e417298fc287985c7f770e83cd2702cb08078c1a
Arsan Dev <tganteng@gmail.com>
2019-10-29 21:05:01 +0700

    commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express'
 const app = express()
+const router = express.Router()
 import axios from 'axios'
 import path from 'path'
 import translate from '@k3rn31p4nic/google-translate-api'
 
-app.use('/', (req,res)=>{
-  res.sendFile(path.join(__dirname, 'index.html'))
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'))
 })
 
-app.use('/random/:lang', (req,res)=>{
+app.get('/random/:lang', (req,res)=>{
   axios.get('https://api.quotable.io/random').then(resp=>{
     translate(resp.data.content, { to: req.params.lang }).then(respo => {
       res.json({quote:respo.text,by:resp.data.author})
